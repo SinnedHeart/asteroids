0939adf3bcfb7a8002ff0e3d41097c4e50ab0b24
SinnedHeart <irrealco@gmail.com>
2025-07-06 04:12:17 -0600

    Player movement and rotation implemented.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
 from constants import *
-
+from player import Player
 # source .venv/bin/activate
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -12,8 +18,16 @@ def main():
                 pygame.quit()
                 return
 
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # Limit the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
